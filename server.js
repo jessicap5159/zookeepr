f61e4240b4aad26a5733fb89ecb94ea7de9cb6ad
jessicap5159 <jessicaj5159@gmail.com>
2021-04-07 15:07:42 -0700

    front end work

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static('public'));
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true}));
 // parse incoming JSON data
@@ -103,6 +105,22 @@ app.post('/api/animals', (req, res) => {
   }
 });
 
+app.get('/', (req,res) => { // our server reads this file and sends it back to client
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
