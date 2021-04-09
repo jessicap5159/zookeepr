2489e97cc35e74a9c48917061715b15b121f9825
jessicap5159 <jessicaj5159@gmail.com>
2021-04-09 11:00:11 -0700

    front end functionality for zookeepers

---
+++ routes/apiRoutes/animalRoutes.js
@@ -0,0 +1,41 @@
+const { filterByQuery, findById, createNewAnimal, validateAnimal } = require('../../lib/animals');
+const { animals } = require('../../data/animals');
+const router = require('express').Router();
+
+router.get('/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+  });
+  
+  router.get('/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
+
+  // Set up a route on our server that accepts data to be used or stored server-side
+// POST requests represent the action of a client requesting the server to accept data
+// POST requests package up data and send it to the server
+router.post('/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+ 
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+ 
+  }
+});
+
+module.exports = router;
+
+
